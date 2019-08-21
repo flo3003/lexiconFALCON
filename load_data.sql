bd30b86a7874bf27a22c682e54f55a8c993af0f1
Flora <32013954+flo3003@users.noreply.github.com>
2019-08-21 22:25:03 +0300

    Update load_data.sql

--- load_data.sql
+++ load_data.sql
@@ -4,4 +4,8 @@ LOAD DATA LOCAL INFILE 'glove-python/coo_matrix.csv' INTO TABLE coo_matrix FIELD
 
 LOAD DATA LOCAL INFILE 'glove-python/word_mapping.csv' INTO TABLE word_mapping FIELDS TERMINATED BY ',' ENCLOSED BY '' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'glove-python/word_mapping.csv' INTO TABLE word_mapping FIELDS TERMINATED BY ',' ENCLOSED BY '' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+/* Uncomment one of the following depending on your choice of the lexicon file */;
+
+-- LOAD DATA LOCAL INFILE 'glove-python/ppdb.csv' INTO TABLE ppdb FIELDS TERMINATED BY ',' ENCLOSED BY '' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+-- LOAD DATA LOCAL INFILE 'glove-python/framenet.csv' INTO TABLE framenet FIELDS TERMINATED BY ',' ENCLOSED BY '' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+-- LOAD DATA LOCAL INFILE 'glove-python/wordnet.csv' INTO TABLE wordnet FIELDS TERMINATED BY ',' ENCLOSED BY '' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
