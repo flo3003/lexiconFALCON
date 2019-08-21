1a6a7ec661e163b17defa00ffdbc008760e7b8bc
Flora <32013954+flo3003@users.noreply.github.com>
2019-08-21 21:06:49 +0300

    Create run_algos_pretrained.sh

---
+++ run_algos_pretrained.sh
@@ -0,0 +1,16 @@
+####### Parameters #######
+host="host_name"
+user="user"
+pass="password"
+database="database_name"
+emb_dim="embedding_dimensions"
+epochs="number_of_epochs"
+emb_file="input_files/pretrainedembeddingsfile_weights.txt"
+
+##### LexiconFALCON #####
+lexicon="lexicon"
+dP=dP_value
+xi=xi_value
+
+######### Train #########
+./lexiconfalcon $host $user $pass $database $lexicon $emb_dim $dP $xi $epochs $emb_file
