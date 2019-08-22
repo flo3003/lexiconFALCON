b5757f0608f387a807934843eac0dea768383c9b
Flora <32013954+flo3003@users.noreply.github.com>
2019-08-22 18:02:50 +0300

    Update analogy.sh

--- experiments/analogy_evaluation/analogy.sh
+++ experiments/analogy_evaluation/analogy.sh
@@ -1,22 +1,21 @@
 function larger_mv ()
-{       while read line1; 
+{       while read line1;
 do
 
 fname="$line1"
 fbname=$(basename "$fname" .txt)
-echo "Doing... $fbname" | tee -a analogy_report 
+echo "Doing... $fbname" | tee -a analogy_report
 
 cat $fname | awk '{for (i=2;i<=NF;i++) printf("%s ",$i); printf("\n");}' > "new_"$fbname".txt"
 
-python ~/glove-python/examples/import_txt2glove.py "new_"$fbname".txt" > /dev/null 2>&1
+python ../../glove-python/examples/import_txt2glove.py "new_"$fbname".txt" > /dev/null 2>&1
 
-python ~/glove-python/examples/analogy_tasks_evaluation.py --test ~/glove-python/examples/questions-words.txt --model ~/glove-python/examples/imported_new_$fbname.model --parallelism 10 | tee -a analogy_report 
+python ../../glove-python/examples/analogy_tasks_evaluation.py --test ../../glove-python/examples/questions-words.txt --model imported_new_$fbname.model --parallelism 10 | tee -a modified_analogy_report
 
 echo | tee -a analogy_report 2>&1
-rm new_$fbname.txt ~/glove-python/examples/imported_$fbname.model
+rm new_$fbname.txt imported_new_$fbname.model
 
 done
 }
-
 
 ls -1 ../../embeddings/* | larger_mv
