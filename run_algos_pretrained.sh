367f8021bebd597e86132359aef8e18613892d4a
Flora <32013954+flo3003@users.noreply.github.com>
2019-08-22 15:32:59 +0300

    Update run_algos_pretrained.sh

--- run_algos_pretrained.sh
+++ run_algos_pretrained.sh
@@ -3,8 +3,8 @@ host="host_name"
 user="user"
 pass="password"
 database="database_name"
-emb_dim="embedding_dimensions"
-epochs="number_of_epochs"
+emb_dim=embedding_dimensions
+epochs=number_of_epochs
 emb_file="input_files/pretrainedembeddingsfile_weights.txt"
 
 ##### LexiconFALCON #####
